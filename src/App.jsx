51f870657577ef29d414286f8193f4c72e72f00e
Complexlity <105590967+Complexlity@users.noreply.github.com>
2022-12-05 00:50:26 +0100

    Add character images

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
+import Cards from "./components/Cards"
+import Header from "./components/Header"
+import Scoreboard from "./components/Scoreboard"
+import './index.css'
+
 
 function App() {
   return (
     <div className="App">
+      <Header />
+      <Scoreboard />
+      <Cards />
     </div>
   )
 }
