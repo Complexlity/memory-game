ddddc6c69a3f036759f92dde4944484f80ef773c
Complexlity <105590967+Complexlity@users.noreply.github.com>
2022-12-05 00:33:48 +0100

    Complete card layout

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,23 +1,12 @@
 import fighterGirl from '../assets/fighter-girl.jpg'
 
-const Cards = () => {
-    return ( 
-        <div className="layout-grid grid p-4">
-            <Card />
-        </div>
-     );
-}
- 
-export default Cards;
-
-
 const Card = () => {
     return ( 
-        <div className="card-grid w-[12rem] h-[16rem] grid  bg-red-400 p-2 rounded-2xl overflow-hidden gap-2">
+        <div className="card-grid w-[14rem] h-[16rem] grid  bg-red-400 p-2 rounded-t-3xl rounded-b-2xl overflow-hidden bottom">
             <div className='rounded-2xl overflow-hidden'><img className="object-cover h-[12rem] w-full" src={fighterGirl} alt="fighter" /></div>
-            <p className='text-center bg-green-300 grid items-center'><span>Fighter Girl</span></p>
+            <p className='text-center text-white font-semibold text-2xl grid items-center'><span>Fighter Girl</span></p>
         </div>
      );
 }
  
-// export default Card;
+export default Card;
---
+++ src/components/Cards.jsx
@@ -0,0 +1,19 @@
+import Card from './Card';
+
+
+const Cards = () => {
+    return ( 
+        <div className="grid layout-grid gap-4 p-4">
+            <Card/>
+            <Card/>
+            <Card/>
+            <Card/>
+            <Card/>
+            <Card/>
+            <Card/>
+            <Card/>
+        </div>
+     );
+}
+ 
+export default Cards;
