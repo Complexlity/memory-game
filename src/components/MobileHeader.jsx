8f5b52c9d96d1bfc045891291a9bf3f5f8f34dbe
Complexlity <105590967+Complexlity@users.noreply.github.com>
2022-12-05 20:10:40 +0100

    Add mobile header

---
+++ src/components/MobileHeader.jsx
@@ -0,0 +1,20 @@
+import logo from "../assets/github-light.png";
+import Scoreboard from "./Scoreboard";
+
+const MobileHeader = ({ score, bestScore }) => {
+  return (
+    <div className="mb-4 pt-4 sm:hidden">
+      <div className="mb-4 flex items-center px-6">
+        <h1 className="main-header flex-1 text-center text-2xl text-gray-200">
+          Test Your Memory
+        </h1>
+        <a href="https://github.com/complexlity" target={"_blank"}>
+          <img className="w-[3rem]" src={logo} alt="" />
+        </a>
+      </div>
+      <Scoreboard score={score} bestScore={bestScore} />
+    </div>
+  );
+};
+
+export default MobileHeader;
