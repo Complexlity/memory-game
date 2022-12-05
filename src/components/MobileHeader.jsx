248d94870bc38f93bbcaab8480435d945e79a84f
Complexlity <105590967+Complexlity@users.noreply.github.com>
2022-12-05 20:17:12 +0100

    Fix spacing

--- src/components/MobileHeader.jsx
+++ src/components/MobileHeader.jsx
@@ -4,12 +4,12 @@ import Scoreboard from "./Scoreboard";
 const MobileHeader = ({ score, bestScore }) => {
   return (
     <div className="mb-4 pt-4 sm:hidden">
-      <div className="mb-4 flex items-center gap-4 px-6">
-        <h1 className="main-header flex-1 text-center text-2xl text-gray-200">
+      <div className="mb-4 flex items-center gap-2 px-6">
+        <h1 className="main-header flex-1 text-center text-xl text-gray-200">
           Test Your Memory
         </h1>
         <a href="https://github.com/complexlity" target={"_blank"}>
-          <img className="w-[3rem]" src={logo} alt="" />
+          <img className="w-[2.5rem]" src={logo} alt="" />
         </a>
       </div>
       <Scoreboard score={score} bestScore={bestScore} />
