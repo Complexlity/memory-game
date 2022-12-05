2371fab0b731810b3cc88cbf3c5a3c4bd3290219
Complexlity <105590967+Complexlity@users.noreply.github.com>
2022-12-05 01:06:37 +0100

    Modify image positioning

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,10 +1,9 @@
-import fighterGirl from '../assets/fighter-girl.jpg'
 
-const Card = () => {
+const Card = ({title, avatar}) => {
     return ( 
         <div className="card-grid w-[14rem] h-[16rem] grid  bg-red-400 p-2 rounded-t-3xl rounded-b-2xl overflow-hidden bottom">
-            <div className='rounded-2xl overflow-hidden'><img className="object-cover h-[12rem] w-full" src={fighterGirl} alt="fighter" /></div>
-            <p className='text-center text-white font-semibold text-2xl grid items-center'><span>Fighter Girl</span></p>
+            <div className='rounded-2xl overflow-hidden'><img className="object-top object-cover  h-[12rem] w-full" src={avatar} alt="fighter" /></div>
+            <p className='text-center text-white font-semibold text-2xl grid items-center'><span>{title}</span></p>
         </div>
      );
 }
--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,17 +1,24 @@
 import Card from './Card';
-
+import naruto from '../assets/naruto.jpg'
+import aang from '../assets/aang.jpg'
+import alita from '../assets/alita.jpg'
+import benTen from '../assets/ben10.jpg'
+import dLuff from '../assets/d-luff.jpg'
+import optimusPrime from '../assets/optimus-prime.jpg'
+import woody from '../assets/woody.jpg'
+import fighterGirl from '../assets/fighter-girl.jpg'
 
 const Cards = () => {
     return ( 
         <div className="grid layout-grid gap-4 p-4">
-            <Card/>
-            <Card/>
-            <Card/>
-            <Card/>
-            <Card/>
-            <Card/>
-            <Card/>
-            <Card/>
+            <Card title='Naruto' avatar={naruto} />
+            <Card title='Aang' avatar={aang}/>
+            <Card title='Alita' avatar={alita}/>
+            <Card title='Ben 10' avatar={benTen}/>
+            <Card title='D Luffy' avatar={dLuff}/>
+            <Card title='Optimus Prime' avatar={optimusPrime}/>
+            <Card title='Woody' avatar={woody}/>
+            <Card title='fighterGirl' avatar={fighterGirl}/>
         </div>
      );
 }
