d6dab66b4b1be61703d44f9e233e352b617954ac
Complexlity <105590967+Complexlity@users.noreply.github.com>
2022-12-05 14:35:46 +0100

    Add more animations and hover styles

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,20 +3,17 @@ import { motion } from "framer-motion";
 
 const Card = ({ id, title, avatar, makeSelected }) => {
   const variants = {
-    visible: { opacity: 1 },
-    hidden: { opacity: 0 },
+    visible: { scale: 1, rotate: 360, opacity: 1 },
+    hidden: { rotate: 180, scale: 0, opacity: 0 },
   };
   return (
     <motion.div
-      className="card-grid bottom grid h-[16rem]  w-[14rem] overflow-hidden rounded-t-3xl rounded-b-2xl bg-red-400 p-2"
+      className="card-grid  bottom grid h-[16rem] w-[14rem]  overflow-hidden rounded-t-3xl rounded-b-2xl bg-red-400 p-2"
       onClick={makeSelected.bind(this, id)}
       initial="hidden"
       animate="visible"
       variants={variants}
-      whileHover={{
-        scale: 1.05,
-        transtion: { duration: 0.8 },
-      }}
+      transition={{ ease: "easeOut", duration: 1.05 }}
     >
       <div className="overflow-hidden rounded-2xl">
         <img
