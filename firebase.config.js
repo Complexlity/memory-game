285742a841378f38ac670cef1ae7596cfa0a1359
Complexlity <105590967+Complexlity@users.noreply.github.com>
2023-01-09 16:59:15 +0100

    Add firebase config

---
+++ firebase.config.js
@@ -0,0 +1,23 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+const ENV = import.meta.env;
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: ENV.VITE_API_KEY,
+  authDomain: ENV.VITE_AUTH_DOMAIN,
+  projectId: ENV.VITE_PROJECT_ID,
+  storageBucket: ENV.VITE_STORAGE_BUCKET,
+  messagingSenderId: ENV.VITE_MESSAGING_SENDER_ID,
+  appId: ENV.VITE_APP_ID,
+  measurementId: ENV.MEASUREMENT_ID,
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+export { app, analytics };
