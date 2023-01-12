97364055f9647cb406269ba37b10183f8f6b91cc
Complexlity <105590967+Complexlity@users.noreply.github.com>
2023-01-12 14:36:34 +0100

    Modify header text layout

--- src/components/Redirect.jsx
+++ src/components/Redirect.jsx
@@ -2,11 +2,11 @@ const Redirect = () => {
   return (
     <div className="flex flex-col justify-center gap-2 bg-zinc-900 py-3 text-center font-mono text-sm sm:flex-row sm:text-lg">
       <p className="text-white">Try out a preview of the new login feature</p>
-      <p className=" flex gap-2 text-yellow-400">
+      <p className=" justify-centers mx-auto flex items-center gap-2 text-yellow-400">
         <span className="text-xl">{"->"}</span>
         <a
           href="https://complexlity-beta-memory-game.netlify.app"
-          className="cursor-pointer text-blue-400 hover:underline"
+          className="cursor-pointer text-center text-blue-400 hover:underline"
         >
           beta.memorygame.com
         </a>
