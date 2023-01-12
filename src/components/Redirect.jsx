5ec3fbffdcf9e62afc7e81962805b06e34f3c586
Complexlity <105590967+Complexlity@users.noreply.github.com>
2023-01-12 02:47:04 +0100

    Remove material ui icons

--- src/components/Redirect.jsx
+++ src/components/Redirect.jsx
@@ -1,11 +1,9 @@
-import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
-
 const Redirect = () => {
   return (
     <div className="flex flex-col justify-center gap-2 bg-zinc-900 py-3 text-center font-mono text-sm sm:flex-row sm:text-lg">
       <p className="text-white">Try out a preview of the new login feature</p>
       <p className=" text-yellow-400">
-        <ArrowForwardIcon />{" "}
+        <span className="text-xl">{"->"}</span>
         <a
           href="https://complexlity-beta-memory-game.netlify.app"
           className="cursor-pointer text-blue-400 hover:underline"
