df4e160dfad5cc822e202ccb36beae4f058ce0e2
Complexlity <105590967+Complexlity@users.noreply.github.com>
2023-01-12 16:01:53 +0100

    Fix redirect layout

--- src/components/Redirect.jsx
+++ src/components/Redirect.jsx
@@ -1,8 +1,8 @@
 const Redirect = () => {
   return (
-    <div className="flex flex-col justify-center gap-2 bg-zinc-900 py-3 text-center font-mono text-sm sm:flex-row sm:text-lg">
-      <p className="text-white">Try out the new login feature</p>
-      <p className=" justify-centers mx-auto flex items-center gap-2 text-yellow-400">
+    <div className="flex flex-col justify-center gap-1 bg-zinc-900 py-2 text-center font-mono text-base sm:block sm:space-x-2 sm:py-3 sm:text-lg">
+      <p className="text-white sm:inline">Try out the new login feature</p>
+      <p className=" mx-auto inline-flex items-center justify-center gap-2 text-yellow-400">
         <span className="text-xl">{"->"}</span>
         <a
           href="https://complexlity-beta-memory-game.netlify.app"
