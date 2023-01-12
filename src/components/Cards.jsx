e04e09954cfedde2e5aa40faa257ebd47b904955
Complexlity <105590967+Complexlity@users.noreply.github.com>
2023-01-12 02:05:04 +0100

    Create back to main site button

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,11 +1,18 @@
 import Card from "./Card";
+import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
 
 const Cards = ({ cards, makeSelected }) => {
   return (
     <div>
       <h2 className="text-center italic text-green-100">
-        <p className="inline text-green-400">RULE</p>: Try not to click any
-        character twice
+        <p className="mb-2 flex cursor-pointer justify-center text-orange-200 hover:underline">
+          <ArrowBackIosIcon />
+          <span className="-ml-2">Back to Main Site</span>{" "}
+        </p>
+        <p>
+          <span className="inline text-green-400">RULE</span>: Try not to click
+          any character twice
+        </p>
       </h2>
       <div className="layout-grid grid justify-center justify-items-center gap-4 p-4">
         {cards.map((card) => (
