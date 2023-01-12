8633b317deacfcf0ec34d6e4d9e0f61b5d1cdd89
Complexlity <105590967+Complexlity@users.noreply.github.com>
2023-01-12 02:48:31 +0100

    Improve redirect spacing

--- src/components/Redirect.jsx
+++ src/components/Redirect.jsx
@@ -2,7 +2,7 @@ const Redirect = () => {
   return (
     <div className="flex flex-col justify-center gap-2 bg-zinc-900 py-3 text-center font-mono text-sm sm:flex-row sm:text-lg">
       <p className="text-white">Try out a preview of the new login feature</p>
-      <p className=" text-yellow-400">
+      <p className=" flex gap-2 text-yellow-400">
         <span className="text-xl">{"->"}</span>
         <a
           href="https://complexlity-beta-memory-game.netlify.app"
